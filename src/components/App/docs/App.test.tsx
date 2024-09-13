02a97643a42ab79fd5a765f0c29d2a91e3059a2f
Jordan Brunn <redstonetechnic8@gmail.com>
2024-09-13 20:33:36 +0200

    fix: Mocked MatchMedia from window object

--- src/components/App/docs/App.test.tsx
+++ src/components/App/docs/App.test.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
+import { mockMatchMedia } from '../../../mockData/window';
 
-test('renders main app', () => {
-  expect(render(<App />)).not.toBeNull();
+describe('App', () => {
+  mockMatchMedia();
+  
+  it('Renders', () => {
+    expect(render(<App />)).not.toBeNull();
+  });
 });
--- src/components/NavbarLeft/docs/NavbarLeft.test.tsx
+++ src/components/NavbarLeft/docs/NavbarLeft.test.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { mockMatchMedia } from "../../../mockData/window";
 import NavbarLeft from "../NavbarLeft";
 import { ThemeProvider } from "styled-components";
 import theme from "../../../assets/themes/default";
 
 describe("NavbarLeft", () => {
+  mockMatchMedia();
+  
   it("renders", () => {
     expect(render(<ThemeProvider theme={theme}><NavbarLeft /></ThemeProvider>)).not.toBeNull();
   });
